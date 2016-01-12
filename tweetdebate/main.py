0f85191f0e3417343e62c5a64a70d69ed54740fc
jrgrafton <jrgrafton@jrgrafton-macbookpro3.roam.corp.google.com>
2016-01-12 01:10:02 -0800

    First working Twitter user stream.
    Unfortunately needed to change local AppEngine files to get it to work:
    
    http://stackoverflow.com/questions/16192916/importerror-no-module-named-
    ssl-with-dev-appserver-py-from-google-app-engine/24066819
    
    And add the following to app.yaml:
    
    libraries:
    - name: ssl
      version: latest
    
    env_variables:
      GAE_USE_SOCKETS_HTTPLIB : True

--- tweetdebate/main.py
+++ tweetdebate/main.py
@@ -1,4 +1,29 @@
 """`main` is the top level module for your Flask application."""
+"""
+import os
+import logging
+
+def is_development_server():
+    return os.environ['APPLICATION_ID'].startswith('dev~')
+
+# See http://stackoverflow.com/questions/16192916/importerror-no-module-named-ssl-with-dev-appserver-py-from-google-app-engine/24066819. The order of all of
+# these things matters!
+if is_development_server():
+    logging.info("is_development_server: setting up local SSL")
+    import sys
+    import imp
+    import inspect
+
+    from google.appengine.tools.devappserver2.python import sandbox
+    sandbox._WHITE_LIST_C_MODULES += ['_ssl', '_socket']
+
+    real_os_src_path = os.path.realpath(inspect.getsourcefile(os))
+    psocket = os.path.join(os.path.dirname(real_os_src_path), 'socket.py')
+    logging.info("is_development_server: %s", psocket)
+    imp.load_source('socket', psocket)
+    phttplib = os.path.join(os.path.dirname(real_os_src_path), 'httplib.py')
+    imp.load_source('httplib', phttplib)
+"""
 from tweetdebate import app
 
 # Note: We don't need to call run() since our application is embedded within
--- tweetdebate/tasks/twitter_stream.py
+++ tweetdebate/tasks/twitter_stream.py
@@ -8,14 +8,14 @@ import logging
 
 class StdOutListener(StreamListener):
     """ A listener handles tweets that are received from the stream.
-    This is a basic listener that just prints received tweets to stdout.
+    This is a basic listener that just prints received tweets to logger.
     """
     def on_data(self, data):
-        print(data)
+        logging.info("on_data: %s", str(data))
         return True
 
     def on_error(self, status):
-        print(status)
+        logging.info("on_error: %s", str(status))
 
 class TwitterStream():
     """ A TwitterStream class that will continuously capture data from a
@@ -23,23 +23,24 @@ class TwitterStream():
     """
     # Go to http://apps.twitter.com and create an app.
     # The consumer key and secret will be generated for you after
-    consumer_key=""
-    consumer_secret=""
+    consumer_key="FGqGxjbjnf8fCWs0ak7j3jlbs"
+    consumer_secret="Gi2hHxSSnlTKDBosxkmvcwN08Ac4vRrw1giRxBruixJo5bApWx"
 
     # After the step above, you will be redirected to your app's page.
     # Create an access token under the the "Your access token" section
-    access_token=""
-    access_token_secret=""
+    access_token="4746890490-3eb19fbYiyigdE7cSuFL1vBxOG9Nf5RBFqcCyWM"
+    access_token_secret="z81oANy3Uyywvvi36rOK3UYJoqvxKI1TACc44MOBx4pSI"
 
     def start(self):
-        logging.info('Starting')
-        return
+        logging.info('start:')
         l = StdOutListener()
-        auth = OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_token_secret)
+        auth = OAuthHandler(self.consumer_key, self.consumer_secret)
+        auth.set_access_token(self.access_token, self.access_token_secret)
 
         stream = Stream(auth, l)
-        stream.filter(track=['#yes','#no'], replies=all)
+        stream.userstream(_with='user',
+                          stall_warnings=True,
+                          async=False)
 
     def is_running(self):
         return False
