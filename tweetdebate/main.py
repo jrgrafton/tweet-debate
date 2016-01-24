c61c0f33d6782f1260eeceae62bfc74431eed7d9
Daniel Rodriguez <itsalllowercasenospaces@gmail.com>
2016-01-23 18:02:00 -0800

    Adding a basic bootstrap template on /home

--- tweetdebate/main.py
+++ tweetdebate/main.py
@@ -1,5 +1,11 @@
 """`main` is the top level module for your Flask application."""
 from tweetdebate import app
+from flask import render_template
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
 # Insert default config to Database
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
