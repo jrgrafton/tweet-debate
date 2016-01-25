c702c8f146ff5d99d1065a12863dce2cdc0a9036
Daniel Rodriguez <itsalllowercasenospaces@gmail.com>
2016-01-24 21:22:31 -0800

    Adding a new SVG interactive map

---
+++ tweetdebate/static/js/utils.js
@@ -0,0 +1,51 @@
+
+var BOOTSTRAP_MIN_SCREEN_WIDTH = {
+  'xs': 256,
+  'sm': 768,
+  'md': 992,
+  'lg': 1200,
+};
+
+var USA_MAP_SIZE_FOR_SCREEN_SIZE = {
+  'xs': 320,
+  'sm': 768,
+  'md': 992,
+  'lg': 1200,
+};
+
+function findBootstrapScreenSize() {
+    var envs = ['xs', 'sm', 'md', 'lg'];
+
+    var $el = $('<div>');
+    $el.appendTo($('body'));
+
+    for (var i = envs.length - 1; i >= 0; i--) {
+        var env = envs[i];
+
+        $el.addClass('hidden-'+env);
+        if ($el.is(':hidden')) {
+            $el.remove();
+            return env;
+        }
+    }
+};
+
+function buildMap(element, width) {
+  var height = 630/930 * width;
+  element.width(width);
+  element.height(height);
+  element.empty();
+  element.usmap({
+    'showLabels': true,
+    'stateStyles': {
+      fill: '#025',
+      "stroke-width": 1,
+      'stroke' : '#036'
+    },
+    'stateHoverStyles': {
+      fill: '#900000'
+    },
+    'click' : function(event, data) {
+    }
+  });
+};
